cb6411ce24e1affbeab7e7db3f2c29c857b6ad52
Trevor VanDoren <trevor@trevorvandoren.com>
2021-03-08 19:55:11 -0600

    initial setup, added Start

---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Start extends Component {
+    render() {
+        return (
+            <p>Testing</p>
+        )
+    }
+}
+
+export default Start;
