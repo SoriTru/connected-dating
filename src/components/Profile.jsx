2df78e0bcf512851bccc054407510289af003d0a
Trevor VanDoren <vandoren.trevor@gmail.com>
2021-03-22 17:34:53 -0500

    added photo carousel

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -13,6 +13,102 @@ class Profile extends Component {
           Town, ST
         </p>
         <p className={styles.basic}>Age / G / O</p>
+        <section className={styles.carousel} aria-label="Gallery">
+          <ol className={styles.carousel_viewport}>
+            <li
+              id="carousel_slide1"
+              tabIndex={0}
+              className={styles.carousel_slide}
+            >
+              <div className={styles.carousel_snapper}>
+                <a href="#carousel_slide4" className={styles.carousel_prev}>
+                  Go to last slide
+                </a>
+                <a href="#carousel_slide2" className={styles.carousel_next}>
+                  Go to next slide
+                </a>
+              </div>
+            </li>
+            <li
+              id="carousel_slide2"
+              tabIndex={0}
+              className={styles.carousel_slide}
+            >
+              <div className={styles.carousel_snapper}>
+                <a href="#carousel_slide1" className={styles.carousel_prev}>
+                  Go to last slide
+                </a>
+                <a href="#carousel_slide3" className={styles.carousel_next}>
+                  Go to next slide
+                </a>
+              </div>
+            </li>
+            <li
+              id="carousel_slide3"
+              tabIndex={0}
+              className={styles.carousel_slide}
+            >
+              <div className={styles.carousel_snapper}>
+                <a href="#carousel_slide2" className={styles.carousel_prev}>
+                  Go to last slide
+                </a>
+                <a href="#carousel_slide4" className={styles.carousel_next}>
+                  Go to next slide
+                </a>
+              </div>
+            </li>
+            <li
+              id="carousel_slide4"
+              tabIndex={0}
+              className={styles.carousel_slide}
+            >
+              <div className={styles.carousel_snapper}>
+                <a href="#carousel_slide3" className={styles.carousel_prev}>
+                  Go to last slide
+                </a>
+                <a href="#carousel_slide1" className={styles.carousel_next}>
+                  Go to next slide
+                </a>
+              </div>
+            </li>
+          </ol>
+          <aside className={styles.carousel_nav}>
+            <ol className={styles.carousel_nav_list}>
+              <li className={styles.carousel_nav_item}>
+                <a
+                  href="#carousel_slide1"
+                  className={styles.carousel_nav_button}
+                >
+                  Go to slide 1
+                </a>
+              </li>
+              <li className={styles.carousel_nav_item}>
+                <a
+                  href="#carousel_slide2"
+                  className={styles.carousel_nav_button}
+                >
+                  Go to slide 2
+                </a>
+              </li>
+              <li className={styles.carousel_nav_item}>
+                <a
+                  href="#carousel_slide3"
+                  className={styles.carousel_nav_button}
+                >
+                  Go to slide 3
+                </a>
+              </li>
+              <li className={styles.carousel_nav_item}>
+                <a
+                  href="#carousel_slide4"
+                  className={styles.carousel_nav_button}
+                >
+                  Go to slide 4
+                </a>
+              </li>
+            </ol>
+          </aside>
+        </section>
       </div>
     );
   }
