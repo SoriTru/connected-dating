d87a46d2973de3054b2d47f05e1c3205fff94bd9
Trevor VanDoren <vandoren.trevor@gmail.com>
2021-03-30 10:36:51 -0500

    added framework for building nav

---
+++ src/components/BottomNavigator/BottomNavigator.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// if you need 'react-native' like it says in the dependency, go to terminal in the project directory and run
+// 'npm add react-native', and then you *should* have access to it
+// then you can uncomment this line below to get the things you probably need
+// import { View, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Animated, Text, Alert } from 'react-native';
+
+class BottomNavigator extends Component {
+  // supporting function go here
+
+  render() {
+    return (
+      <div>
+        {/*most of the code you write will go here,
+                 and this div changes to a 'View', I think*/}
+      </div>
+    );
+  }
+}
