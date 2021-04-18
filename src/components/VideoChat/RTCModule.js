cef4b16d0ce41f903bcf97fe8f1563a3c1e1c219
Trevor VanDoren <vandoren.trevor@gmail.com>
2021-04-18 16:49:39 -0500

    added in turn server

--- src/components/VideoChat/RTCModule.js
+++ src/components/VideoChat/RTCModule.js
@@ -38,7 +38,19 @@ export const initiateConnection = async () => {
     // create a connection
     // using Google public stun server
     const configuration = {
-      iceServers: [{ urls: "stun:stun2.1.google.com:19302" }],
+      iceServers: [
+        { url: ["stun:stun.1.google.com:19302"] },
+        { url: "stun:stun.l.google.com:19302" },
+        { url: "stun:stun1.l.google.com:19302" },
+        { url: "stun:stun2.l.google.com:19302" },
+        { url: "stun:stun3.l.google.com:19302" },
+        { url: "stun:stun4.l.google.com:19302" },
+        {
+          url: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+      ],
     };
 
     return new RTCPeerConnection(configuration);
